fa1a7a4e57ddcfdea18176f711080fa15b3886f3
raykaris <ryanmuthoni050@gmail.com>
2026-04-24 10:55:32 -0700

    update

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+
+const authRouter = Router();
+
+authRouter.post('/sign-up', (req, res) => {
+    res.send({ title: 'Sign-up' });
+});
+
+export default authRouter;
