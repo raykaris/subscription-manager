ae9f122490183f60c4ada0617e1562d8a6e1664f
raykaris <ryanmuthoni050@gmail.com>
2026-04-27 07:47:27 -0700

    update

---
+++ frontend/app/subscriptions/[id].tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const subscriptionDetails = () => {
+  return (
+    <View>
+      <Text>subscription Details</Text>
+    </View>
+  )
+}
+
+export default subscriptionDetails
